27018fd49552002fcfe6050720d6226179c3dfa9
Ian Perfitt <141587134+ianperfitt@users.noreply.github.com>
2024-05-27 12:14:02 -0500

    fist commit

---
+++ src/main/java/com/example/flyway_practice/FlywayPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.flyway_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlywayPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlywayPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/flyway_practice/FlywayPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.flyway_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlywayPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
